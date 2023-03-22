4c38692c11dd38524aa8163f83f4648ec613b805
rcshpark <rcshpark@gmail.com>
2023-03-22 22:25:11 +0900

    googleMap sample

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyCoxeZ8hvuWrw6HZLLrLh0V-A3BeNfG0No")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
