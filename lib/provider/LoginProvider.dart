0b71372ba13f02bac0a7567f1af65d0d634de504
rcshpark <rcshpark@gmail.com>
2023-03-29 22:07:31 +0900

    test logic

--- lib/provider/LoginProvider.dart
+++ lib/provider/LoginProvider.dart
@@ -8,12 +8,20 @@ import 'package:http/http.dart' as http;
 enum TargetPage { main, login }
 
 String loginUrl = Platform.isAndroid
-    ? 'http://10.0.2.2:3000/api'
-    : 'http://127.0.0.1:3000/api';
+    ? 'http://10.0.2.2:3000/users'
+    : 'http://127.0.0.1:3000/users';
 kakaoLoginApi(String code) async {
   try {
     final url = Uri.parse('$loginUrl?code=$code');
-  } catch (e) {}
+    http.Response response = await http.post(url, headers: <String, String>{
+      'Content-Type': 'application/x-www-form-urlencoded',
+    }, body: {
+      "code": code,
+    });
+    print(response);
+  } catch (e) {
+    print('e:$e');
+  }
 }
 
 class LoginProvider with ChangeNotifier {
@@ -23,50 +31,47 @@ class LoginProvider with ChangeNotifier {
   Future<String?> kakaoLogin() async {
     if (await isKakaoTalkInstalled()) {
       try {
-        await UserApi.instance.loginWithKakaoTalk();
-        String? name = await checkLogin();
+        OAuthToken code = await UserApi.instance.loginWithKakaoTalk();
+        String? name = await checkLogin(code.accessToken);
         _targetPage = TargetPage.main;
-        notifyListeners();
 
         return name;
       } catch (error) {
         _targetPage = TargetPage.login;
-        notifyListeners();
 
         // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
         // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
         if (error is PlatformException && error.code == 'CANCELED') {
           _targetPage = TargetPage.login;
-          notifyListeners();
+
           return null;
         }
         // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
         try {
-          await UserApi.instance.loginWithKakaoAccount();
-          String? name = await checkLogin();
+          OAuthToken code = await UserApi.instance.loginWithKakaoAccount();
+          String? name = await checkLogin(code.accessToken);
           _targetPage = TargetPage.main;
-          notifyListeners();
+
           return name;
         } catch (error) {
           _targetPage = TargetPage.login;
-          notifyListeners();
         }
       }
     } else {
       try {
-        await UserApi.instance.loginWithKakaoAccount();
-        String? name = await checkLogin();
+        OAuthToken code = await UserApi.instance.loginWithKakaoAccount();
+        String? name = await checkLogin(code.accessToken);
         _targetPage = TargetPage.main;
-        notifyListeners();
 
         return name;
       } catch (error) {
         _targetPage = TargetPage.login;
         print(error);
-        notifyListeners();
       }
     }
   }
 
-  Future<String?> checkLogin() async {}
+  Future<String?> checkLogin(String code) async {
+    final response = await kakaoLoginApi(code);
+  }
 }
