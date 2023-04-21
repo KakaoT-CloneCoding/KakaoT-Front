e9b61b887055892ec9d155aa6c82e2b862c03074
rcshpark <rcshpark@gmail.com>
2023-04-21 22:35:37 +0900

    feat: 검색결과 리스트 별 버튼 생성

--- lib/widget/search_list_widget.dart
+++ lib/widget/search_list_widget.dart
@@ -74,3 +74,98 @@ searchListWidget(String place_name, String address_name, String input_text) {
     ),
   );
 }
+
+resultListWidget(String place_name, String address_name, String input_text) {
+  TextSpan getColoredText() {
+    if (place_name.contains(input_text)) {
+      String beforeKeyword =
+          place_name.substring(0, place_name.indexOf(input_text));
+      String keyword = place_name.substring(place_name.indexOf(input_text),
+          place_name.indexOf(input_text) + input_text.length);
+      String afterKeyword = place_name
+          .substring(place_name.indexOf(input_text) + input_text.length);
+
+      return TextSpan(
+        children: [
+          TextSpan(text: beforeKeyword),
+          TextSpan(
+            text: keyword,
+            style: const TextStyle(color: Colors.blue),
+          ),
+          TextSpan(text: afterKeyword),
+        ],
+      );
+    } else {
+      return TextSpan(text: place_name);
+    }
+  }
+
+  return Padding(
+    padding: const EdgeInsets.all(15.0),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            RichText(
+              text: TextSpan(
+                style: const TextStyle(color: Colors.black),
+                children: [
+                  getColoredText(),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Row(
+              children: [
+                const Text(
+                  '거리',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                  ),
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                const Text(
+                  '|',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 14,
+                  ),
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                Text(address_name,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                    ))
+              ],
+            )
+          ],
+        ),
+        ElevatedButton(
+          onPressed: () {
+            print('##123 button on ');
+          },
+          style: ElevatedButton.styleFrom(
+              fixedSize: const Size(30, 20),
+              backgroundColor: Colors.white,
+              elevation: 0,
+              side: const BorderSide(width: 0, color: Colors.grey)),
+          child: const Text(
+            '도착',
+            style: TextStyle(fontSize: 12, color: Colors.black),
+          ),
+        )
+      ],
+    ),
+  );
+}
