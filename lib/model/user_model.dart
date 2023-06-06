e1d81ad0b899156c71f2a53e9798ce58b6eae23a
rcshpark <rcshpark@gmail.com>
2023-06-06 16:53:21 +0900

    유저 모델 수정

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,15 +1,19 @@
 class UserModel {
+  late dynamic id;
   late String email;
   late String nickname;
   late String profile_image_url;
+  late String access_token;
   // late String token;
 
-  UserModel({email, nickname, profile_image_url});
+  UserModel({id, email, nickname, profile_image_url, access_token});
 
   UserModel.fromJson(Map<String, dynamic> json) {
+    id = json["id"];
     email = json["email"];
     nickname = json["nickname"];
     profile_image_url = json["profile_image_url"];
+    access_token = json["access_token"];
     // token = json["access_token"];
   }
 }
